ab38c3d571c7d655cdfa9ed584836dfc303931f1
Raul Libório <rauhmaru@gmail.com>
2016-04-15 02:46:14 -0300

    Added files via upload
    Adição de scripts básicos

---
+++ batbot.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+# BaTbot current version
+VERSION="1.2"
+
+# default token and chatid
+# or run BaTbot with option: -t <token>
+TELEGRAMTOKEN="158977230:AAH_5bEPsC6WO2QCQl9RvMegNRUvm1RiShs";
+
+# how many seconds between check for new messages
+# or run Batbot with option: -c <seconds>
+CHECKNEWMSG=5;
+
+HELPCOMANDOS="/uptime - Executa o comando uptime no servidor\n
+/grafico _ITEMID_ - Retorna o gráfico da última hora do ITEMID\n
+/ping _endereço_ - Executa o ping para o endereço\n
+/nslookup _endereço_- Executa o nslookup para o endereço\n
+/help - Exibe esta tela de ajuda"
+
+# Commands
+# you have to use this exactly syntax: ["/mycommand"]='<system command>'
+# please, don't forget to remove all example commands!
+declare -A botcommands
+botcommands=(
+
+
+	["/uptime"]="uptime"
+
+	["/grafico (.*)"]="bash tg.sh @R1"
+	
+	["/ping (.*)"]="ping -c 3 @R1"
+
+	["/nslookup (.*)"]="nslookup @R1"
+
+	["/dolar"]="dolar"
+
+	["/disco"]="df -h"
+
+	["/help"]="echo -e $HELPCOMANDOS"
+
+)
+
+# + end config
+# +
+# +
+# +
+
+FIRSTTIME=0;
+BOTPATH="`dirname \"$0\"`";
+
+echo "+"
+while getopts :ht:c: OPTION; do
+	case $OPTION in
+		h)
+			echo " BaTbot: Bash Telegram Bot"
+			echo "+"
+			echo " Usage: ${0} [-t <token>] [-c <seconds>]"
+			exit;
+		;;
+		t)
+			echo "Set Token to: ${OPTARG}";
+			TELEGRAMTOKEN=$OPTARG;
+		;;
+		c)
+			echo "Check for new messages every: ${OPTARG} seconds";
+			CHECKNEWMSG=$OPTARG;
+		;;
+	esac
+done
+echo "+"
+
+echo -e "\nInitializing BaTbot v${VERSION}"
+ABOUTME=`curl -s "https://api.telegram.org/bot${TELEGRAMTOKEN}/getMe"`
+if [[ "$ABOUTME" =~ \"ok\"\:true\, ]]; then
+	if [[ "$ABOUTME" =~ \"username\"\:\"([^\"]+)\" ]]; then
+		echo "Username: ${BASH_REMATCH[1]}";
+	fi
+
+	if [[ "$ABOUTME" =~ \"first_name\"\:\"([^\"]+)\" ]]; then
+		echo "First name: ${BASH_REMATCH[1]}";
+	fi
+
+	if [[ "$ABOUTME" =~ \"id\"\:([0-9\-]+), ]]; then
+		echo "Bot ID: ${BASH_REMATCH[1]}";
+		BOTID=${BASH_REMATCH[1]};
+	fi
+
+else
+	echo "Error: maybe wrong token... exit.";
+	exit;
+fi
+
+if [ -e "${BOTPATH}/${BOTID}.lastmsg" ]; then
+	FIRSTTIME=0;
+else
+	touch ${BOTPATH}/${BOTID}.lastmsg;
+	FIRSTTIME=1;
+fi
+
+echo -e "Done. Waiting for new messages...\n"
+
+while true; do
+	MSGOUTPUT=$(curl -s "https://api.telegram.org/bot${TELEGRAMTOKEN}/getUpdates");
+	MSGID=0;
+	TEXT=0;
+	FIRSTNAME="";
+	LASTNAME="";
+	echo "${MSGOUTPUT}" | while read -r line ; do
+		if [[ "$line" =~ \"chat\"\:\{\"id\"\:([\-0-9]+)\, ]]; then
+			CHATID=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"message\_id\"\:([0-9]+)\, ]]; then
+			MSGID=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"text\"\:\"(.+)\"\}\} ]]; then
+			TEXT=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"username\"\:\"([^\"]+)\" ]]; then
+			USERNAME=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"first_name\"\:\"([^\"]+)\" ]]; then
+			FIRSTNAME=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"last_name\"\:\"([^\"]+)\" ]]; then
+			LASTNAME=${BASH_REMATCH[1]};
+		fi
+
+		if [[ "$line" =~ \"from\"\:\{\"id\"\:([0-9\-]+), ]]; then
+			FROMID=" ${BASH_REMATCH[1]}";
+		fi
+
+
+		if [[ $MSGID -ne 0 && $CHATID -ne 0 ]]; then
+			LASTMSGID=$(cat "${BOTID}.lastmsg");
+			if [[ $MSGID -gt $LASTMSGID ]]; then
+				echo "[chat ${CHATID}, from ${FROMID}] <${USERNAME} - ${FIRSTNAME} ${LASTNAME}> ${TEXT}";
+				echo $MSGID > "${BOTID}.lastmsg";
+
+				for s in "${!botcommands[@]}"; do
+					if [[ "$TEXT" =~ \\${s} ]]; then
+						CMDORIG=${botcommands["$s"]};
+						CMDORIG=${CMDORIG//@USERID/$FROMID};
+						CMDORIG=${CMDORIG//@USERNAME/$USERNAME};
+						CMDORIG=${CMDORIG//@FIRSTNAME/$FIRSTNAME};
+						CMDORIG=${CMDORIG//@LASTNAME/$LASTNAME};
+						CMDORIG=${CMDORIG//@CHATID/$CHATID};
+						CMDORIG=${CMDORIG//@MSGID/$MSGID};
+						CMDORIG=${CMDORIG//@TEXT/$TEXT};
+						CMDORIG=${CMDORIG//@FROMID/$FROMID};
+						CMDORIG=${CMDORIG//@R1/${BASH_REMATCH[1]}};
+						CMDORIG=${CMDORIG//@R2/${BASH_REMATCH[2]}};
+						CMDORIG=${CMDORIG//@R3/${BASH_REMATCH[3]}};
+
+						echo "Command ${s} received, running cmd: ${CMDORIG}"
+						CMDOUTPUT=`$CMDORIG`;
+
+						if [ $FIRSTTIME -eq 1 ]; then
+							echo "old message, i will not send any answer to user.";
+						else
+							curl -s -d "text=${CMDOUTPUT}&chat_id=${CHATID}" "https://api.telegram.org/bot${TELEGRAMTOKEN}/sendMessage" > /dev/null
+						fi
+					fi
+				done
+			fi
+		fi
+	done
+
+	FIRSTTIME=0;
+
+	read -t $CHECKNEWMSG answer;
+	if [[ "$answer" =~ ^\.msg.([\-0-9]+).(.*) ]]; then
+		CHATID=${BASH_REMATCH[1]};
+		MSGSEND=${BASH_REMATCH[2]};
+		curl -s -d "text=${MSGSEND}&chat_id=${CHATID}" "https://api.telegram.org/bot${TELEGRAMTOKEN}/sendMessage" > /dev/null;
+	elif [[ "$answer" =~ ^\.msg.([a-zA-Z]+).(.*) ]]; then
+		CHATID=${BASH_REMATCH[1]};
+		MSGSEND=${BASH_REMATCH[2]};
+		curl -s -d "text=${MSGSEND}&chat_id=@${CHATID}" "https://api.telegram.org/bot${TELEGRAMTOKEN}/sendMessage" > /dev/null;
+	fi
+
+done
+
+exit 0
---
+++ tg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Alertas do zabbix no telegram
+# Raul Liborio, rauhmaru@opensuse.org
+#
+# Carregar as principais variaveis 
+. $(dirname "$0")/vars.conf
+
+# Telegram
+# Customizacao do gráfico 
+# É possível mudar a cor e tipo do gráfico
+# drawtype = 0 - linha fina, 1 - preenchido, 2 - linha grossa, 3 - pontilhado, 4 - pontilhado fino, 5 - gradiente
+# color = Cor do grafico, em hexadecimal
+
+URL="${ZBX_SERVER}/chart3.php?&width=900&height=200&period=3600&name=${ZBX_ITEMID}&legend=1&items[0][itemid]=${ZBX_ITEMID}&items[0][drawtype]=5&items[0][color]=B173F0"
+
+# Comandos
+CURL="curl -s"
+CHATID=$( ${CURL} ${TELEGRAM_URL}${GETUPDATE} | awk -F'"chat":{"id":' END'{print $2}' | sed 's/,.*//' )
+MENSAGEM=$1
+
+# Login no Zabbix
+login() {
+    # Crie um cookie de autenticacao
+    ${CURL} --cookie-jar ${TMP_COOKIE} --request POST --data "name=${ZBX_USER}&password=${ZBX_PASS}&enter=Sign%20in" ${ZBX_SERVER}/
+}
+
+get_image() {
+    # Faz download do grafico em png e o salva em um diretorio temporario
+    ${CURL} --cookie ${TMP_COOKIE} --globoff "${URL}" -o ${IMG_NAME}
+}
+
+# Envio de mensagens de texto
+#${CURL} ${TELEGRAM_URL}${SENDMESSAGE} -F "chat_id=${CHATID}" -F "text=${MENSAGEM}" 
+
+# Envio de graficos
+login
+get_image "${URL}" ${IMG_NAME}
+${CURL} ${TELEGRAM_URL}/sendPhoto -F "chat_id=${CHATID}" -F "photo=@${IMG_NAME}" &> /dev/null
+# rm -f ${IMG_NAME}
