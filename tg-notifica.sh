648e81fcb2bf70e6782d4bf5cf76707e7716b369
Raul Libório <rauhmaru@gmail.com>
2016-04-15 03:08:30 -0300

    Update tg-notifica.sh
    Correção de parâmetros

--- tg-notifica.sh
+++ tg-notifica.sh
@@ -11,11 +11,5 @@ CURL="curl -s"
 CHATID=$( ${CURL} ${TELEGRAM_URL}${GETUPDATE} | awk -F'"chat":{"id":' END'{print $2}' | sed 's/,.*//' )
 MENSAGEM=$1
 
-# Login no Zabbix
-login() {
-    # Crie um cookie de autenticacao
-    ${CURL} --cookie-jar ${TMP_COOKIE} --request POST --data "name=${ZBX_USER}&password=${ZBX_PASS}&enter=Sign%20in" ${ZBX_SERVER}/
-}
-
 # Envio de mensagens de texto
 ${CURL} ${TELEGRAM_URL}${SENDMESSAGE} -F "chat_id=${CHATID}" -F "text=${MENSAGEM}" 
