46574736843a0138038c951c4d1d26189c4d318c
Raul Libório <rauhmaru@gmail.com>
2016-04-18 11:46:08 -0300

    Create dolar.sh
    Script que consulta o valor corrente do dólar

---
+++ dolar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Consulta o valor do dolar
+# Raul Liborio, rauhmaru@opensuse.org
+DOLAR=/tmp/dolar
+CURL=$(curl -s "http://developers.agenciaideias.com.br/cotacoes/json" > ${DOLAR})
+echo "$(cat ${DOLAR} | awk -F'"' '{ print $14": " $16}')"
+echo "$(cat ${DOLAR} | awk -F'"' '{ print $18": " $20}')"
