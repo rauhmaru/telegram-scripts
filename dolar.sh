1d61dee85c35955c3e7a5dce3e89531bfc09d265
Raul Libório <rauhmaru@gmail.com>
2016-04-22 15:57:02 -0300

    Update dolar.sh
    Mudança no retorno. Ao invés de cotacao, retorna dolar na linha 01

--- dolar.sh
+++ dolar.sh
@@ -3,5 +3,5 @@
 # Raul Liborio, rauhmaru@opensuse.org
 DOLAR=/tmp/dolar
 CURL=$(curl -s "http://developers.agenciaideias.com.br/cotacoes/json" > ${DOLAR})
-echo "$(cat ${DOLAR} | awk -F'"' '{ print $14": " $16}')"
+echo "$(cat ${DOLAR} | awk -F'"' '{ print $12": " $16}')"
 echo "$(cat ${DOLAR} | awk -F'"' '{ print $18": " $20}')"
