ea9e40939475e84d87898436a75da25fc668cd82
Raul Libório <rauhmaru@gmail.com>
2016-05-20 14:01:28 -0300

    URL não funciona

--- dolar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Consulta o valor do dolar
-# Raul Liborio, rauhmaru@opensuse.org
-DOLAR=/tmp/dolar
-CURL=$(curl -s "http://developers.agenciaideias.com.br/cotacoes/json" > ${DOLAR})
-echo "$(cat ${DOLAR} | awk -F'"' '{ print $12": " $16}')"
-echo "$(cat ${DOLAR} | awk -F'"' '{ print $18": " $20}')"
