d338cd5e56d5bfed02fed3f3bc89b0383946a5ae
Raul Libório <rauhmaru@gmail.com>
2019-12-06 09:47:14 -0300

    Update tg-grafico.sh

--- tg-grafico.sh
+++ tg-grafico.sh
@@ -4,7 +4,7 @@
 # Raul Liborio, rauhmaru@opensuse.org
 #
 # Carregar as principais variaveis 
-. $(dirname "$0")/conf/vars.py
+. $(dirname "$0")/conf/vars.conf
 
 #Cria o TMP_DIR
 [ ! -d "${TMP_DIR}" ] && (mkdir -p ${TMP_DIR} || TMP_DIR="/tmp")
