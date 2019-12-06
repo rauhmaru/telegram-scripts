5598f5b77f93bc15cc976b52ab7108dfb67735a3
Raul Libório <rauhmaru@gmail.com>
2019-12-06 09:48:42 -0300

    Delete lerolero.sh

--- lerolero.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Lero lero
-# AUTHOR: Raul Libório, rauhmaru@opensuse.org
-# O script acessa o lerolero.com e puxa a frase da homepage. E só.
-
-curl -s http://www.lerolero.com | grep blockquote | sed 's/<[^>]*>//g'
