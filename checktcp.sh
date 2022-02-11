d34d7f43f6bf0b23df520f98217645f7a322c7f3
Raul Libório <rauhmaru@gmail.com>
2022-02-11 12:00:46 -0300

    Update checktcp.sh
    update in bash builtin

--- checktcp.sh
+++ checktcp.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # CHECKTCP - Verifica portas TCP
 # Raul Liborio, rauhmaru@opensuse.org
-# Uso:
-# checktcp.sh HOST PORTAS
-# 
-# EXEMPLOS:
-# checktcp.sh HOST {1..1024} - Verifica da porta 1 a 2014
-# checktcp.sh HOST 21 22 23 53 - Verifica da porta 1 a 2014
-[ $# -eq "1" ] && echo -e "ERRO NO USO.\nUso: $(basename $0) HOST PORTAS" && exit 1
+
+Exemplo_err="ERRO NO USO.\nUso: ${0##*/} [host|IP] portas\n\n
+
+EXEMPLOS:\n
+ ${0##*/} HOST {1..1024} - Verifica da porta 1 a 2014\n
+ ${0##*/} HOST 21 22 23 53 - Verifica da porta 1 a 2014\n"
+
+[ $# -eq "1" ] && echo -e $Exemplo_err  && exit 1
 
 echo "Verificando portas TCP em $1..."
-for PORT in $@;
-do
+
+for PORT in $@; do
   (echo >/dev/tcp/${1}/${PORT}) >/dev/null 2>&1 && echo "${PORT} aberta"
 done
